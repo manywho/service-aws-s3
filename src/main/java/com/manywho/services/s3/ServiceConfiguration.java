41db03ff01e6388ac6cfbc20a5ca726b6f81ce8c
jose <jose.collazzi@manywho.com>
2016-12-08 10:58:29 +0000

    change to use passwords instead of strings in the configuration

--- src/main/java/com/manywho/services/s3/ServiceConfiguration.java
+++ src/main/java/com/manywho/services/s3/ServiceConfiguration.java
@@ -4,13 +4,13 @@ import com.manywho.sdk.api.ContentType;
 import com.manywho.sdk.services.configuration.Configuration;
 
 public class ServiceConfiguration implements Configuration {
-    @Configuration.Setting(name="Bucket Name", contentType = ContentType.String)
+    @Configuration.Setting(name="Bucket Name", contentType = ContentType.Password)
     private String bucketName;
 
-    @Configuration.Setting(name="AWS Access Key Id", contentType = ContentType.String)
+    @Configuration.Setting(name="AWS Access Key Id", contentType = ContentType.Password)
     private String awsAccessKeyId;
 
-    @Configuration.Setting(name="AWS Secret Access Key", contentType = ContentType.String)
+    @Configuration.Setting(name="AWS Secret Access Key", contentType = ContentType.Password)
     private String awsSecretAccessKey;
 
     public String getBucketName() {
